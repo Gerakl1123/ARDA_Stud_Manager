82032539378bcf716b5da490b50e8c0413859674
Gerakl1123 <germanniyazgg@gmail.com>
2025-07-11 01:17:39 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include"mainwindow.h"
@@ -34,61 +32,3 @@ private slots:
 
 };
 #endif // MAINWINDOW_H
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-#include"mainwindow.h"
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
-
-private slots:
-
-};
-#endif // MAINWINDOW_H
->>>>>>> 7acdfd58b9ccc84905bc36da7d0a7cba70d1e82e
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-#include"mainwindow.h"
-
-#include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
-
-private slots:
-
-};
-#endif // MAINWINDOW_H
->>>>>>> 7acdfd58b9ccc84905bc36da7d0a7cba70d1e82e
